a0699282c65f8a28dbab6efd8848721abb71b498
bolbass <ann.bolbass@gmail.com>
2022-08-12 17:09:50 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -37,8 +37,36 @@ const MORSE_TABLE = {
     '-----':  '0',
 };
 
-function decode(expr) {
-    // write your solution here
+function decode(str) {
+    let arr = [];
+    let res = '';
+    let element;
+    let str10;
+    let str2;
+    const dash = '11';
+    const dot = '10';
+    if (str.length % 10 !== 0) return 0;
+    for (let i = 0; i < str.length; i += 10) {
+        element = '';
+        str10 = str.slice(i, i + 10);
+        if (str10 === '**********') {
+            arr.push(' ');
+            continue;
+        }
+        for (let j = 0; j < str10.length; j += 2) {
+            str2 = str10.slice(j, j + 2);
+            str2.includes(dot)?element += ".":str2.includes(dash)? element += "-" : element += "";
+        }
+        arr.push(element);
+    }
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] === ' ') { 
+            res += arr[i];
+            continue;
+        }
+        res += MORSE_TABLE[arr[i]];
+    }
+    return res;
 }
 
 module.exports = {
